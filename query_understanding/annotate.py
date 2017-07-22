ff6095c82a2659734860df50013db7e0c76f2f75
DataEngg <kunal1340@outlook.com>
2017-07-22 21:27:18 +0530

    Added file based query

--- query_understanding/annotate.py
+++ query_understanding/annotate.py
@@ -18,10 +18,10 @@ class Annotater(object):
         """
         Initialize variable for tokenizing
         """
-        self.data_path = os.path.join(os.path.abspath(os.path.join("..", os.path.abspath('..'))), 'dataset')
+        self.data_path = os.path.join("test_dataset")
         self.csv_path = os.path.join(self.data_path)
 
-    def annotate(self, color=True, price=True, gender=True,shop=True):
+    def annotate(self, color=True, price=True, gender=True, shop=True):
         """
         Tokenize the sentence.
         """
@@ -50,6 +50,36 @@ class Annotater(object):
                     print(traceback.format_exc())
                     print("No Mapping")
 
+    def test_annotate(self, color=True, price=True, gender=True, shop=True):
+        """
+        Tokenize the sentence.
+        """
+        file_open = open(os.path.join(self.csv_path, "answer.csv"),'w')
+        with open(os.path.join(self.csv_path, "by.csv")) as fout:
+            for query in fout.readlines():
+                if query:
+                    file_open.write(query + "--->>")
+                    try:
+                        value = {}
+                        list_token = [item.lower().strip() for item in query.lower().split(' ')]
+                        if color:
+                            value, list_token = Colors().finding_colors(list_tokens=list_token)
+                        query = " ".join(list_token)
+                        if price:
+                            query, value = Price().finding_price(string=query, val=value)
+                        if gender:
+                            query, value = Gender().finding_gender(query, val=value)
+                        if shop:
+                            query, value = Shop().finding_shop(query, val=value)
+                        val = Term().finding_term(query, val=value)
+                        # print(query)
+                        file_open.write(str(val) + "\n")
+                    except Exception as e:
+                        import traceback
+                        print(traceback.format_exc())
+                        print("No Mapping")
+
 
 if __name__ == '__main__':
-    Annotater().annotate(color=True,gender=True)
+    # Annotater().annotate(color=True,gender=True,price=True,shop=True)
+    Annotater().test_annotate(color=True, gender=True, price=True, shop=True)
--- query_understanding/shop.py
+++ query_understanding/shop.py
@@ -11,19 +11,18 @@ class Shop(object):
     def shop(self):
         with open(os.path.join(self.csv_path, "shop_name.csv")) as fout:
             for data in fout.readlines():
-                self.s.add(data.strip().lower())
+                for d in data.strip().lower().split(' '):
+                    self.s.add(d)
 
     def finding_shop(self, query, val):
         val['shop'] = []
-        print(query)
-        print(self.s)
         list_tokens = query.split('by')
-        print(list_tokens)
-        print(len(list_tokens))
         if len(list_tokens) == 2:
             if list_tokens[1].strip().lower() in self.s:
                 val['shop'].append(list_tokens[1].strip().lower())
                 return list_tokens[0], val
+            list_tokens.append(list_tokens[1])
+            list_tokens[1] = 'by'
             return " ".join(list_tokens), val
         elif len(list_tokens) == 1:
             return " ".join(list_tokens), val
