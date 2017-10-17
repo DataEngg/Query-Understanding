75bd17f71ae755a28510b1f86bcb24b66b6e0839
DataEngg <kunal1340@outlook.com>
2017-10-17 16:20:33 +0530

    change in code for querying

--- query_understanding/annotate.py
+++ query_understanding/annotate.py
@@ -23,48 +23,41 @@ class Annotater(object):
         self.csv_path = os.path.join(self.data_path)
         self.spell = SpellCheck()
 
-    def annotate(self, color=True, price=True, gender=True, shop=True, size=True):
+    def annotate(self, query='', color=True, price=True, gender=True, shop=True, size=True):
         """
         Tokenize the sentence.
         """
 
-        while (True):
-            print("Enter the query ")
-            query = input()
-            if query:
-                try:
-                    value = {}
-                    query = re.sub(' +', ' ', query)
-                    query = query + " "
-                    list_token = [item.lower().strip() for item in query.lower().split(' ')]
-                    spell_query = []
-                    is_spell = False
-                    for token in list_token:
-                        spell_word = self.spell.run(token)
-                        spell_query.append(spell_word if '#' not in spell_word else spell_word.split('#')[1])
-                    original_query = " ".join(list_token)
-                    new_query = " ".join(spell_query)
-                    if color:
-                        value, list_token = Colors().finding_colors(list_tokens=list_token)
-                    query = " ".join(list_token)
-                    if original_query != new_query:
-                        is_spell = True
-                    if size:
-                        query, value = Size().finding_size(query, value)
-                    if price:
-                        query, value = Price().finding_price(string=query, val=value)
-                    if gender:
-                        query, value = Gender().finding_gender(query, val=value)
-                    if shop:
-                        query, value = Shop().finding_shop(query, val=value)
-                    val = Term().finding_term(query, val=value)
-                    val['original_query'] = original_query
-                    val['did_you_mean'] = new_query
-                    val['is_spell'] = is_spell
-                    print(val)
-                except Exception as e:
-                    print("No Mapping")
-
-
-if __name__ == '__main__':
-    Annotater().annotate(color=True, gender=True, price=True, shop=True, size=True)
+        if query:
+            try:
+                value = {}
+                query = re.sub(' +', ' ', query)
+                query = query + " "
+                list_token = [item.lower().strip() for item in query.lower().split(' ')]
+                spell_query = []
+                is_spell = False
+                for token in list_token:
+                    spell_word = self.spell.run(token)
+                    spell_query.append(spell_word if '#' not in spell_word else spell_word.split('#')[1])
+                original_query = " ".join(list_token)
+                new_query = " ".join(spell_query)
+                if color:
+                    value, list_token = Colors().finding_colors(list_tokens=list_token)
+                query = " ".join(list_token)
+                if original_query != new_query:
+                    is_spell = True
+                if size:
+                    query, value = Size().finding_size(query, value)
+                if price:
+                    query, value = Price().finding_price(string=query, val=value)
+                if gender:
+                    query, value = Gender().finding_gender(query, val=value)
+                if shop:
+                    query, value = Shop().finding_shop(query, val=value)
+                val = Term().finding_term(query, val=value)
+                val['original_query'] = original_query
+                val['did_you_mean'] = new_query
+                val['is_spell'] = is_spell
+                print(val)
+            except Exception as e:
+                print("No Mapping")
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 import os
 
+def read(f):
+    return open(os.path.abspath(f)).read().strip()
+
 datadir = os.path.join('query_understanding','dataset')
+
 datafiles = [(d, [os.path.join(d, f) for f in files])
              for d, folders, files in os.walk(datadir)]
 print(datafiles)
 setup(
     name='query_understanding',
     packages=['query_understanding'],
-    version='1.0.0',
+    version='1.0.3',
     description='Universal encoding detector',
     author='Kunal Gupta and Moghira Rahman',
     author_email='kunal1340@iiitd.ac.in',
